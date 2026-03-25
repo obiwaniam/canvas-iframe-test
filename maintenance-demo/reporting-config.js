05112beb427d54557c433faeada9f5e8c9562231
obiwaniam <obiwaniam@pm.me>
2026-03-25 13:41:17 -0700

    Wire Google Sheets reporting endpoint for maintenance demo scores

--- maintenance-demo/reporting-config.js
+++ maintenance-demo/reporting-config.js
@@ -1,4 +1,4 @@
 // Companion reporting config that lives with the page.
 // Point these to your server endpoints (same host recommended).
-window.MAINTENANCE_REPORT_ENDPOINT = '';
-window.MAINTENANCE_RESULTS_ENDPOINT = '';
+window.MAINTENANCE_REPORT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxmi-4JJHZf7jetgvzKvPDU_BP90-A9aBdFz1XI9pCU9ckLKPT503I28gP1Bc_pLXA/exec';
+window.MAINTENANCE_RESULTS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxmi-4JJHZf7jetgvzKvPDU_BP90-A9aBdFz1XI9pCU9ckLKPT503I28gP1Bc_pLXA/exec?action=results';

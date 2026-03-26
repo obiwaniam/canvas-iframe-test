60f1f3c88796798598c511bf348fa327a5f169a8
obiwaniam <obiwaniam@pm.me>
2026-03-25 19:24:06 -0700

    Update to new Apps Script deployment URL

--- maintenance-demo/reporting-config.js
+++ maintenance-demo/reporting-config.js
@@ -1,4 +1,4 @@
 // Companion reporting config that lives with the page.
 // Point these to your server endpoints (same host recommended).
-window.MAINTENANCE_REPORT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxmi-4JJHZf7jetgvzKvPDU_BP90-A9aBdFz1XI9pCU9ckLKPT503I28gP1Bc_pLXA/exec';
-window.MAINTENANCE_RESULTS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxmi-4JJHZf7jetgvzKvPDU_BP90-A9aBdFz1XI9pCU9ckLKPT503I28gP1Bc_pLXA/exec?action=results';
+window.MAINTENANCE_REPORT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbzhIJdB0-W0WiyaSj25d2uYZwPRIhQZbZoAO8uBKMsfyh6nrh8yrEsYiHgnDpIoGjAd/exec';
+window.MAINTENANCE_RESULTS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbzhIJdB0-W0WiyaSj25d2uYZwPRIhQZbZoAO8uBKMsfyh6nrh8yrEsYiHgnDpIoGjAd/exec?action=results';

a36aa1d19ffba0d4a8f2b76841ae505f46a29a5b
obiwaniam <obiwaniam@pm.me>
2026-03-25 20:25:08 -0700

    Move attempt tracking to client-side, use generic Apps Script endpoint

--- maintenance-demo/reporting-config.js
+++ maintenance-demo/reporting-config.js
@@ -1,4 +1,4 @@
 // Companion reporting config that lives with the page.
 // Point these to your server endpoints (same host recommended).
-window.MAINTENANCE_REPORT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbzhIJdB0-W0WiyaSj25d2uYZwPRIhQZbZoAO8uBKMsfyh6nrh8yrEsYiHgnDpIoGjAd/exec';
-window.MAINTENANCE_RESULTS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbzhIJdB0-W0WiyaSj25d2uYZwPRIhQZbZoAO8uBKMsfyh6nrh8yrEsYiHgnDpIoGjAd/exec?action=results';
+window.MAINTENANCE_REPORT_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyNz8Eps2G_gmhNM8xhIMNXGYagUKFjTjBGfp7B6gwk8ch0ilDFHATVKWpA5EyU9meZ/exec';
+window.MAINTENANCE_RESULTS_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyNz8Eps2G_gmhNM8xhIMNXGYagUKFjTjBGfp7B6gwk8ch0ilDFHATVKWpA5EyU9meZ/exec?action=results';
